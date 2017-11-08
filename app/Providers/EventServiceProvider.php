c1fdca01709bc88ed4484f2f04839963673ad183
Santosh <santosh_potu@epam.com>
2017-11-08 16:03:16 +0530

    second commit

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SantoshLaravel\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'SantoshLaravel\Events\Event' => [
+            'SantoshLaravel\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('foo',function(){
+    return "my route";
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('ID/{id?}',function($id = 0){
+    return "ID {$id}";
+});
+
+Route::get('welcome/{locale?}',function($locale){
+          App::setLocale($locale);
+          return view('translation_example');
+    
+});
